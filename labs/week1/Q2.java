92ebdf78784ac93f9a005a8d32c0a98fd53788ca
ruizalberto <albertoruizb14@gmail.com>
2022-09-20 12:44:42 +0100

    Finished Q2

--- labs/week1/Q2.java
+++ labs/week1/Q2.java
@@ -4,22 +4,22 @@ public class Q2 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         
-        int[] number = new int[5];
+        int[] number = {0, 0, 0, 0, 0};
         int i = 0;
-        do {
-            System.out.printf("Enter a number: ");
-            number[i] = input.nextInt();
+        while (i<5){
+            while (number[i] < 1 || number[i] > 30){
+                System.out.printf("Enter a number: ");
+                number[i] = input.nextInt();
+            }
             i++;
-        } while (number[i] > 0 && number[i] < 30);
-        
+        }
     
-        for (int j = 0; j < number[j]; j++)
+        for (int j = 0; j < number.length; j++)
         {
             String asterik = "*";
             System.out.println(asterik.repeat(number[j])); 
         }
                     
-        System.out.println();
         input.close();
     }
 }
