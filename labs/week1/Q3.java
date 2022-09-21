1d91e22fa62b534a8ecd2b37861b05d9ddb1933c
ruizalberto <albertoruizb14@gmail.com>
2022-09-21 15:21:32 +0100

    Finished Q3

--- labs/week1/Q3.java
+++ labs/week1/Q3.java
@@ -1,26 +1,44 @@
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Random;
 
 public class Q3 {
     public static void main(String[] args) {
         int[] flightSeats = new int[10];
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Please Type 1 for First Class and Please Type 2 for Economy: ");
-        int seatClass = input.nextInt();
-        int seatAssigned = 0;
-        int upperBound = 5;
-        Random rand = new Random();
-        if (seatClass == 1){
-            seatAssigned = rand.nextInt(upperBound)+1;
-        } else if (seatClass == 2){
-            seatAssigned = rand.nextInt(upperBound)+6;
+        for (int i = 0; i<flightSeats.length; i++){
+            System.out.print("Please Type 1 for First Class and Please Type 2 for Economy: ");
+            int seatClass = input.nextInt();
+            int seatAssigned = 0;
+            if (seatClass == 1){
+                while (flightSeats[seatAssigned] != 0){
+                    seatAssigned++;
+                }
+            } else if (seatClass == 2){
+                seatAssigned += 5;
+                while (flightSeats[seatAssigned] != 0){
+                    seatAssigned++;
+                    if (seatAssigned==10){
+                        System.out.println("Economic section is full.");
+                        System.out.print("Is it acceptable if we placed you in the First Class section? [yes/no] ");
+                        String reply = input.next();
+                        if (reply.equals("yes")){
+                            seatAssigned = 0;
+                            while (flightSeats[seatAssigned] != 0){
+                                seatAssigned++;
+                            }
+                        }
+                    }
+                }
+            }
+            flightSeats[seatAssigned] = seatAssigned+1;
+            System.out.println("Your boarding pass is " + flightSeats[seatAssigned]);
+            if (seatAssigned+1>5){
+                System.out.println("Economy section of the plane");
+            } else {
+                System.out.println("First Class section of the plane");
+            }
         }
-        flightSeats[seatAssigned] = seatAssigned;
-        System.out.println("Your boarding pass is " + seatAssigned);
-        if (seatAssigned>5){System.out.println("Economy section of the plane");} 
-        else {System.out.println("First Class section of the plane");}
-        
         input.close();
     }
 }
