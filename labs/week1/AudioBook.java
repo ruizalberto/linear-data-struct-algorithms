9a2f93f9743cd31970559b6bddabe7b8fc11d096
ruizalberto <albertoruizb14@gmail.com>
2022-09-27 10:25:35 +0100

    Made a big mixed

--- labs/week1/AudioBook.java
+++ labs/week1/AudioBook.java
@@ -1,11 +1,18 @@
-public class AudioBook extends Book{
+public class AudioBook extends Book {
     String sizeBook;
     String playLength;
     String playbackName;
 
+    public AudioBook(){
+        super();
+        this.sizeBook = "";
+        this.playLength = "";
+        this.playbackName = "";
+    }
+
     @Override
     public String toString() {
-        return super.toString() + "with a size of " + this.sizeBook + 
+        return super.toString() + " with a size of " + this.sizeBook + 
         ", a play length of " + this.playLength + " and a playback artist's name of " + this.playbackName;
     }
 }
--- labs/week1/Book.java
+++ labs/week1/Book.java
@@ -13,4 +13,81 @@ public class Book {
     public String toString() {
         return this.title + " was made by " + this.author + " on " + this.yearOfPublication;
     }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYearOfPublication(String yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYearOfPublication() {
+        return yearOfPublication;
+    }
+}
+
+class PrintBook extends Book {
+    String publisher;
+    String ISBN;
+
+    public PrintBook() {
+        super();
+        this.publisher = "";
+        this.ISBN = "";
+    }
+
+    public void setISBN(String iSBN) {
+        this.ISBN = iSBN;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ". Publisher's name " + getPublisher() + ". ISBN: " + getISBN();
+    }
+
+}
+
+class Test {
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.setAuthor("alberto");
+        book.setTitle("Title Test");
+        book.setYearOfPublication("2022");
+
+        System.out.println(book.toString());
+
+        PrintBook printBook = new PrintBook();
+        printBook.setPublisher("Hiii");
+        printBook.setISBN("sdfs");
+        printBook.setAuthor("alberto");
+        printBook.setTitle("alberto Title");
+        printBook.setYearOfPublication("2022");
+
+        System.out.println(book.toString());
+    }
 }
