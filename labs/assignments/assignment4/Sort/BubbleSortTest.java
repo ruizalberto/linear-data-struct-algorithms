c3521f26e02a53144c9277c13b8638129fbe6e06
ruizalberto <albertoruizb14@gmail.com>
2022-11-28 17:45:20 +0000

    Finish BubbleSortTest java class

--- labs/assignments/assignment4/Sort/BubbleSortTest.java
+++ labs/assignments/assignment4/Sort/BubbleSortTest.java
@@ -16,6 +16,14 @@ public class BubbleSortTest {
   */              
   private static void bubbleSort(int[] data) {
      //to-complete
+     int n = data.length;
+     for (int i = 0; i < n - 1; i++){
+      for (int j = 0; j < n - i - 1; j++){
+         if (data[j] > data[j + 1]) {
+            swap(data, j, j+1);
+         }
+      }   
+     }
   } 
 
   /**
