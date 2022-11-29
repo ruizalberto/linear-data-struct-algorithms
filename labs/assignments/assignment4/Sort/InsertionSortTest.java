4efdf424d45107db7416e18454e1dd440d7b0f71
ruizalberto <albertoruizb14@gmail.com>
2022-11-29 11:15:05 +0000

    Fix printpass in insertionsorttest java

--- labs/assignments/assignment4/Sort/InsertionSortTest.java
+++ labs/assignments/assignment4/Sort/InsertionSortTest.java
@@ -19,12 +19,12 @@ class InsertionSortTest {
     for (int i = 1; i < n; ++i) {
         int key = data[i];
         int j = i - 1;
-        printPass(data, i, j);
 
         while (j >= 0 && data[j] > key) {
             data[j + 1] = data[j];
             j = j - 1;
         }
+        printPass(data, i, j+1);
         data[j + 1] = key;
     }
    }
